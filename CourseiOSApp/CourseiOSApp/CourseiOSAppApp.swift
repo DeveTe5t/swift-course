cd5debe39562a289b8596fc14ab8c9ee8128314a
DeveTe5t <devcurses@gmail.com>
2024-06-10 10:32:21 -0600

    Added gitignore

---
+++ CourseiOSApp/CourseiOSApp/CourseiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseiOSAppApp.swift
+//  CourseiOSApp
+//
+//  Created by Totus tuo on 10/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CourseiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
