f0bec3f053f54ab96fd34efbe4835d68ee63b6ad
DeveTe5t <devcurses@gmail.com>
2024-06-10 14:56:33 -0600

    Start swift UI

--- CourseiOSApp/CourseiOSApp/ContentView.swift
+++ CourseiOSApp/CourseiOSApp/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hey, world!")
+            Text("Hey, world!")            
         }
         .padding()
     }
