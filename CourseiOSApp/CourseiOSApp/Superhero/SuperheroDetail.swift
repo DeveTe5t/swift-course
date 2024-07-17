6509f7fa12e50359320cb3f984ef699351e6b0ab
DeveTe5t <devcurses@gmail.com>
2024-07-17 12:58:25 -0600

    Finished superherodetail

--- CourseiOSApp/CourseiOSApp/Superhero/SuperheroDetail.swift
+++ CourseiOSApp/CourseiOSApp/Superhero/SuperheroDetail.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SDWebImageSwiftUI
+import Charts
 
 struct SuperheroDetail: View {
     let id: String
@@ -25,7 +26,7 @@ struct SuperheroDetail: View {
                     .resizable()
 //                    .scaledToFit()
                     .scaledToFill()
-                    .frame(height: 300)
+                    .frame(height: 450)
                     .clipped()
                 
                 Text(superhero.name)
@@ -33,20 +34,28 @@ struct SuperheroDetail: View {
                     .font(.title)
                     .foregroundColor(.white)
                 
-                ForEach(superhero.biography.aliases, id: \.self) { alias in
+//                ForEach(superhero.biography.aliases, id: \.self) { alias in
+                ForEach(superhero.biography.aliases.indices.prefix(2), id: \.self) { index in
+                                                            
+                    let alias = superhero.biography.aliases[index]
                     Text(alias)
                         .foregroundColor(.gray)
                         .italic()
+                    
                 }
                 
-                Text(superhero.biography.fullName)
-                    .foregroundColor(.white)
+//                Text(superhero.biography.fullName)
+//                    .foregroundColor(.white)
                 
                 Text(superhero.biography.publisher)
                     .foregroundColor(.white)
                 
                 Text(superhero.biography.alignment)
+                    .bold()
+                    .font(.title3)
                     .foregroundColor(.white)
+                
+                SuperheroStats(stats: superhero.powerstats)
                 
                 Spacer()
             }
@@ -67,6 +76,70 @@ struct SuperheroDetail: View {
     }
 }
 
+struct SuperheroStats: View {
+    let stats: ApiNetwork.Powerstats
+    
+    var body: some View {
+        VStack {
+            
+            Chart {
+                SectorMark(
+                    angle: .value("Count", Int(stats.combat) ?? 0),
+                    innerRadius: .ratio(0.6), // width of sectormark
+                    angularInset: 5 // separation between sectormark
+                )
+                .cornerRadius(5)
+                .foregroundStyle(by: .value("Category", "Combat"))
+                
+                SectorMark(
+                    angle: .value("Count", Int(stats.durability) ?? 0),
+                    innerRadius: .ratio(0.6),
+                    angularInset: 5
+                )
+                .cornerRadius(5)
+                .foregroundStyle(by: .value("Category", "Durability"))
+                
+                SectorMark(
+                    angle: .value("Count", Int(stats.intelligence) ?? 0),
+                    innerRadius: .ratio(0.6),
+                    angularInset: 5
+                )
+                .cornerRadius(5)
+                .foregroundStyle(by: .value("Category", "Inteligence"))
+                
+                SectorMark(
+                    angle: .value("Count", Int(stats.power) ?? 0),
+                    innerRadius: .ratio(0.6),
+                    angularInset: 5
+                )
+                .cornerRadius(5)
+                .foregroundStyle(by: .value("Category", "Power"))
+                
+                SectorMark(
+                    angle: .value("Count", Int(stats.speed) ?? 0),
+                    innerRadius: .ratio(0.6),
+                    angularInset: 5
+                )
+                .cornerRadius(5)
+                .foregroundStyle(by: .value("Category", "Speed"))
+                
+                SectorMark(
+                    angle: .value("Count", Int(stats.strength) ?? 0),
+                    innerRadius: .ratio(0.6),
+                    angularInset: 5
+                )
+                .cornerRadius(5)
+                .foregroundStyle(by: .value("Category", "Stregth"))
+            }
+        }
+        .padding(16)
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: 350)
+        .background(.white)
+        .cornerRadius(16)
+        .padding(24)
+    }
+}
+
 #Preview {
-    SuperheroDetail(id: "10")
+    SuperheroDetail(id: "70")
 }
