01e3dfa86c239488b612f14ff5ddd94ba1e12473
DeveTe5t <devcurses@gmail.com>
2024-07-16 23:55:06 -0600

    Add some things to detail view

--- CourseiOSApp/CourseiOSApp/Superhero/SuperheroDetail.swift
+++ CourseiOSApp/CourseiOSApp/Superhero/SuperheroDetail.swift
@@ -6,15 +6,67 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct SuperheroDetail: View {
     let id: String
     
+    @State var superhero: ApiNetwork.SuperheroCompleted? = nil
+    @State var loading: Bool = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if loading {
+                ProgressView().tint(.white)
+            }
+//            if superhero is not nil
+            else if let superhero = superhero {
+                WebImage(url: URL(string: superhero.image.url))
+                    .resizable()
+//                    .scaledToFit()
+                    .scaledToFill()
+                    .frame(height: 300)
+                    .clipped()
+                
+                Text(superhero.name)
+                    .bold()
+                    .font(.title)
+                    .foregroundColor(.white)
+                
+                ForEach(superhero.biography.aliases, id: \.self) { alias in
+                    Text(alias)
+                        .foregroundColor(.gray)
+                        .italic()
+                }
+                
+                Text(superhero.biography.fullName)
+                    .foregroundColor(.white)
+                
+                Text(superhero.biography.publisher)
+                    .foregroundColor(.white)
+                
+                Text(superhero.biography.alignment)
+                    .foregroundColor(.white)
+                
+                Spacer()
+            }
+        }
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+        .background(.backgroundApp)
+        .onAppear {
+            Task {
+                do {
+                    superhero = try await ApiNetwork().getHeroById(id: id)
+                } catch {
+                    superhero = nil
+                }
+                
+                loading = false
+            }
+        }
     }
 }
 
 #Preview {
-    SuperheroDetail(id: "1")
+    SuperheroDetail(id: "10")
 }
