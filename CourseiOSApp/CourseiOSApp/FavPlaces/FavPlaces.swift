8a92e75f0c3ab475555a51bef050efbbe5a5f16c
DeveTe5t <devcurses@gmail.com>
2024-07-17 21:51:06 -0600

    Changes in menu and add third app to the menu and start

---
+++ CourseiOSApp/CourseiOSApp/FavPlaces/FavPlaces.swift
@@ -0,0 +1,18 @@
+//
+//  FavPlaces.swift
+//  CourseiOSApp
+//
+//  Created by Totus tuo on 17/07/24.
+//
+
+import SwiftUI
+
+struct FavPlaces: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavPlaces()
+}
